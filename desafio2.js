9da51d83f2cedc636ae0ef82effc86c561fca7a1
Camila Arruda <camilaam1802@gmail.com>
2021-11-05 16:53:20 -0300

    questão 2 operação adição concluida

---
+++ desafio2.js
@@ -0,0 +1,16 @@
+/* Crie um hashmap (ou um objeto simples em JavaScript) que contenha as funções elementares de adição, subtração, multiplicação e divisão.
+Leve em consideração que a divisão não deve permitir dividendo 0
+A função sum permite um array como parâmetro de entrada e adiciona todos os seus elementos.
+A função de subtração permite um array como parâmetro de entrada e subtrai todos os seus elementos.
+Função de multiplicação - Idem
+A função de divisão aceita dois parâmetros: a e b.
+*/
+
+const operationFunctions = {
+    sum: (array) => {
+        return array.reduce((acc,curr) => acc + curr, 0)
+    },
+
+};
+
+console.log(operationFunctions.sum([1, 2, 3, 4, 5, 6]));
